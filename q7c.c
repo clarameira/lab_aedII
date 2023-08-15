2b30f9be36176532348c25824327e9bdc7fad886
Clara Meira <you@example.com>
2023-08-14 22:53:20 -0300

    q7c

---
+++ q7c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int f(int m, int n){
+    if (m == 1) {
+        return n + 1;
+    } else if (n == 1) {
+        return f(m - 1, 2);
+    } else{
+        return f(m, n - 1) + f(m - 1, n);
+    }
+}
+
+int main(){
+    int m, n;
+    printf("Digite o valor de m: ");
+    scanf("%d", &m);
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+    
+    int resultado = f(m, n);
+    printf("O resultado de f(%d, %d) é %d\n", m, n, resultado);
+    
+    return 0;
+}
